5ec94ac54c49f8f7d6885475615c37f9223aaf01
Matias <matiaz.freak@gmail.com>
2014-01-03 16:28:53 -0300

    Main changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,100 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 #include "Facet.hpp"
+#include <vector>
 using namespace std;
 
+
+vector<string> split(string,char);
 
 /*	argv[0] = input_file_name
 	Example:
 		If you used the file called SDSS.dat SDSS_neighbours.dat SDSS_vertex.dat, input_file_name would be SDSS
-*/	
+*/			
 int main(int argc, char **argv){
-
 
 	/*
 		Each file is written as follow:
 
 			input.dat :=	x	y 	z
-				-this file contains the data of points in the space with (x,y,z) in float format.
+				- first line means number of dimensions.
+				- second line means how many points has the file.
+				- this file contains the data of points in the space with (x,y,z) in float format.
 
 			input_neighbours.dat := number_of_neighbours neighbour_id_1 neighbour_id_2 neighbour_id_3 neighbour_id_4
+				- first line means how many facets has the file.
 				- this file contains the data of neighbours of an specific facet. File's line specifies the facet's id.
 
 			input_vertex.dat := number_of_vertexes vertex_id_1 vertex_id_2 vertex_id_3 vertex_id_4
+				- first line means how many facets has the file.			
 				- this file contains the data of vertexes of an specific facet. File's line specifies the facet's id. 
 	*/
 
-	string points = argv[1]+string(".dat");		
-	string neightbours = points+string("_neighbours.dat");
-	string vertex = points+string("_vertex.dat");
+	if(argc < 2){
+		cout << "No input file specified" << '\n';
+		return 0;
+	}
 
-	ifstream points_file(points.c_str());
-	if(!points_file.good()){
-		cout << "Error when tried to open '" << points << "'" << '\n';
+	// Files names into strings
+	string points_file = argv[1]+string(".dat");		
+	string neightbours_file = points_file+string("_neighbours.dat");
+	string vertex_file = points_file+string("_vertex.dat");
+
+	// Crete the points dictionary in array 'points'
+	ifstream points_ifs(points_file.c_str());
+	if(!points_ifs.good()){
+		cout << "Error when tried to open '" << points_file << "'" << '\n';
 		return 0;
 	}else{
-		while(!points_file.eof()){
-			string line;
-			getline(points_file,line);
-			cout << line << '\n' ;
+		// Using the second line to initialize the array
+		string line;
+		getline(points_ifs,line);
+		getline(points_ifs,line);
+		int points_number = atoi(line.c_str());
+		float* points[points_number];
+		// Getting the points in file
+		for(int k = 0; k < points_number ; k++){
+			getline(points_ifs,line);
+			line.replace(line.length(),1,"\t");
+			vector<string> line_splited = split(line,'\t');
+			// Matrix column x y z in points[k]
+			points[k] = new float[3];
+			for(vector<string>::iterator it = line_splited.begin(); it != line_splited.end() ; it++){
+				
+			}
+			//cout << "\n";
 		}
+
 	}
 
 }
+
+vector<string> split(string str, char token){
+	vector<string> result;
+	size_t begin = 0 ,found = 0;
+	found = str.find(token,begin);
+	while(found != string::npos){
+		result.push_back(str.substr(begin,found-begin+1)); 
+		begin = found+1;
+		found = str.find(token,begin);
+	}
+	return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
